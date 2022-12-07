8aba9de3e86ca2439f7b4b33dd9aae90d4616ad3
Neema <patelneema93@gmail.com>
2022-12-07 16:08:30 -0500

    create Login page

--- frontend/todo-app/src/components/todo/TodoApp.jsx
+++ frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -4,7 +4,19 @@ class TodoApp extends Component {
     render(){
         return(
             <div className='TddoApp'>
-                My TodoApp
+                <LoginComponent/>
+            </div>
+        )
+    }
+}
+
+class LoginComponent extends Component{
+    render(){
+        return (
+            <div className='LoginComponent'>
+                UserName: <input type="text" name="username"/>
+                Password: <input type="password" name="password"/>
+                <button type="submit">Login</button>
             </div>
         )
     }
